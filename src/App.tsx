df30ff0ed28477fb5810cffd58731c55adfbaaff
QuestonP <quest@email.unc.edu>
2023-10-08 00:38:44 -0400

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Main Body
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Services
+            Apply 
+            Team and Bio
+            Markets
+            footer
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
